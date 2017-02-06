697311a96a2e76cba027fe8125e9e272f9e4ee20
ohooblueblue <helloworldzhuang@foxmail.com>
2017-02-07 00:22:32 +0800

    项目基本结构搭建

---
+++ app/src/main/java/com/zdd/myassistant/activity/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.zdd.myassistant.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zdd.myassistant.R;
+
+/**
+ * Project Name: MyAssistant
+ * File Name:    LoginActivity.java
+ * ClassName:    LoginActivity
+ * <p>
+ * Description: 登录界面
+ *
+ * @author zdd
+ * @date 2017年02月06日 21:13
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ app/src/main/java/com/zdd/myassistant/activity/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.zdd.myassistant.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zdd.myassistant.R;
+
+/**
+ * Project Name: MyAssistant
+ * File Name:    RegisterActivity.java
+ * ClassName:    RegisterActivity
+ * <p>
+ * Description: 注册界面
+ *
+ * @author zdd
+ * @date 2017年02月05日 21:13
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
