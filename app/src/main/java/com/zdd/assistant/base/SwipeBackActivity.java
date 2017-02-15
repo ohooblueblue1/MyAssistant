b5961ab9dd1373e8982d19285a1c50236151dcb5
庄冬冬 <zhuangdongdong@4399inc.com>
2017-02-15 16:37:49 +0800

    实现Activity侧滑关闭效果

---
+++ app/src/main/java/com/zdd/assistant/base/SwipeBackActivity.java
@@ -0,0 +1,17 @@
+package com.zdd.assistant.base;
+
+/**
+ * Project Name: MyAssistant
+ * File Name:    SwipeBackActivity.java
+ * ClassName:    SwipeBackActivity
+ *
+ * Description: TODO.
+ *
+ * @author ZDD
+ * @date 2017年02月15日 16:29
+ *
+ * Copyright (c) 2017年, 4399 Network CO.ltd. All Rights Reserved.
+ */
+public class SwipeBackActivity
+{
+}
