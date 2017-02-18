b586537c3c59c0de3cefe2532a6bde93ddfc0708
庄冬冬 <zhuangdongdong@4399inc.com>
2017-02-18 14:50:44 +0800

    配置Volley,主界面跳转逻辑

--- app/src/main/java/com/zdd/assistant/activity/main/MainActivity.java
+++ app/src/main/java/com/zdd/assistant/activity/main/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zdd.assistant.R;
 import com.zdd.assistant.base.BaseActivity;
@@ -24,54 +26,80 @@ import com.zdd.assistant.util.ActivityCollector;
  * @author zdd
  * @date 2017年02月05日 21:13
  */
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-
-    // TODO: 2017/2/15 布局上方可展示天气信息
-
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener,
+                                                          View.OnClickListener
+{
     private DrawerLayout mDrawer;
 
+    //天气信息相关
+    private TextView mTvCityName;
+    private TextView mTvUpdateTime;
+    private ImageView mIvWeatherIcon;
+    private TextView mTvTemperature;
+    private TextView mTvWeatherName;
+    //运动指数
+    private TextView mTvSport;
+    //洗车指数
+    private TextView mTvCarWashing;
+    //流感指数
+    private TextView mTvFlu;
+    //紫外线强度
+    private TextView mTvUv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
     }
 
-    private void initView() {
+    private void initView()
+    {
         //设置此页面不能侧滑关闭
         setSwipeBackEnable(false);
         //关闭其他Activity
         ActivityCollector.finishAllOther(this);
-        //初始化toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //初始化toolbar,侧滑菜单
+        initTop();
 
-        //初始化侧滑菜单
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        //设置四个功能tab监听
+        //设置主界面功能tab监听
         findViewById(R.id.rl_tab_cook).setOnClickListener(this);
         findViewById(R.id.rl_tab_guide).setOnClickListener(this);
         findViewById(R.id.rl_tab_notepad).setOnClickListener(this);
         findViewById(R.id.rl_tab_robot).setOnClickListener(this);
+        findViewById(R.id.rl_tab_weather).setOnClickListener(this);
+    }
+
+    private void initTop()
+    {
+        //初始化toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //初始化侧滑菜单
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open,
+                                                                 R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     /**
      * 按下返回键
      */
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }
+        else
+        {
             super.onBackPressed();
         }
     }
@@ -84,17 +112,16 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
      */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        if (id == R.id.nav_camera)
+        {
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_gallery)
+        {
 
         }
 
@@ -102,13 +129,34 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         return true;
     }
 
-    public static void actionStart(Context context) {
+    public static void actionStart(Context context)
+    {
         Intent intent = new Intent(context, MainActivity.class);
         context.startActivity(intent);
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
 
+            case R.id.rl_tab_cook:
+
+                break;
+
+            case R.id.rl_tab_guide:
+
+                break;
+
+            case R.id.rl_tab_notepad:
+
+                break;
+
+            case R.id.rl_tab_robot:
+
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/zdd/assistant/app/MyApplication.java
+++ app/src/main/java/com/zdd/assistant/app/MyApplication.java
@@ -3,6 +3,9 @@ package com.zdd.assistant.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import cn.bmob.v3.Bmob;
 
 /**
@@ -14,46 +17,42 @@ import cn.bmob.v3.Bmob;
  * @author zdd
  * @date 2017年02月12日 0:03
  */
-public class MyApplication extends Application {
+public class MyApplication extends Application
+{
 
-    public static final String APPLICATION_ID = "d4564776a5246d78d181e41bd447ee1d";
+    public static final String BMOB_APPLICATION_ID = "d4564776a5246d78d181e41bd447ee1d";
+
     private static Context sContext;
+    private static RequestQueue mRequestQueue;
+
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
 
         sContext = getApplicationContext();
-        configBmob();
+        //配置bmob后端云
+        Bmob.initialize(this, BMOB_APPLICATION_ID);
+        //配置Volley
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 
-    /**
-     * 配置bmob后端云
-     */
-    private void configBmob() {
-        //提供以下两种方式进行初始化操作：
-        //第一：默认初始化
-        Bmob.initialize(this, APPLICATION_ID);
-
-        //第二:设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
-        //BmobConfig config =new BmobConfig.Builder(this)
-        ////设置appkey
-        //.setApplicationId("Your Application ID")
-        ////请求超时时间（单位为秒）：默认15s
-        //.setConnectTimeout(30)
-        ////文件分片上传时每片的大小（单位字节），默认512*1024
-        //.setUploadBlockSize(1024*1024)
-        ////文件的过期时间(单位为秒)：默认1800s
-        //.setFileExpiration(2500)
-        //.build();
-        //Bmob.initialize(config);
-    }
 
     /**
      * @return 应用程序上下文对象
      */
-    public static Context getAppContext() {
+    public static Context getAppContext()
+    {
         return sContext;
+    }
+
+    /**
+     * @return Volley请求队列
+     */
+    public static RequestQueue getRequestQueue()
+    {
+        return mRequestQueue;
     }
 
 
