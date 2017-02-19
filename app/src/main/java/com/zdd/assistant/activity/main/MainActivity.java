a7386cae1e11086df6bcd63dd3879e1b452587ee
ohooblueblue <helloworldzhuang@foxmail.com>
2017-02-19 14:41:34 +0800

    主界面天气信息获取

--- app/src/main/java/com/zdd/assistant/activity/main/MainActivity.java
+++ app/src/main/java/com/zdd/assistant/activity/main/MainActivity.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 
 import com.zdd.assistant.R;
 import com.zdd.assistant.base.BaseActivity;
+import com.zdd.assistant.entity.weather.WeatherNow;
+import com.zdd.assistant.entity.weather.WeatherSuggestion;
+import com.zdd.assistant.provider.OnResponseListener;
 import com.zdd.assistant.provider.WeatherProvider;
 import com.zdd.assistant.util.ActivityCollector;
 
@@ -27,9 +30,7 @@ import com.zdd.assistant.util.ActivityCollector;
  * @author zdd
  * @date 2017年02月05日 21:13
  */
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener,
-                                                          View.OnClickListener
-{
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     private DrawerLayout mDrawer;
 
     //天气信息相关
@@ -50,23 +51,32 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private WeatherProvider mWeatherProvider;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
         initDataProVider();
+        loadWeatherData();
     }
 
-    private void initView()
-    {
+    private void initView() {
         //设置此页面不能侧滑关闭
         setSwipeBackEnable(false);
+
         //关闭其他Activity
         ActivityCollector.finishAllOther(this);
-        //初始化toolbar,侧滑菜单
-        initTop();
+
+        //初始化toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //初始化侧滑菜单
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //设置主界面功能tab监听
         findViewById(R.id.rl_tab_cook).setOnClickListener(this);
@@ -74,46 +84,100 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         findViewById(R.id.rl_tab_notepad).setOnClickListener(this);
         findViewById(R.id.rl_tab_robot).setOnClickListener(this);
         findViewById(R.id.rl_tab_weather).setOnClickListener(this);
-    }
 
-    private void initTop()
-    {
-        //初始化toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //初始化侧滑菜单
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open,
-                                                                 R.string.navigation_drawer_close);
-        mDrawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        //初始化天气相关View
+        mTvCityName = (TextView) findViewById(R.id.tv_city_name);
+        mTvUpdateTime = (TextView) findViewById(R.id.tv_update_time);
+        mIvWeatherIcon = (ImageView) findViewById(R.id.iv_weather_icon);
+        mTvTemperature = (TextView) findViewById(R.id.tv_temperature);
+        mTvWeatherName = (TextView) findViewById(R.id.tv_weater_name);
+        mTvSport = (TextView) findViewById(R.id.tv_do_sport);
+        mTvCarWashing = (TextView) findViewById(R.id.tv_do_car_washing);
+        mTvFlu = (TextView) findViewById(R.id.tv_do_flu);
+        mTvUv = (TextView) findViewById(R.id.tv_do_uv);
     }
 
 
-    /**
-     * 初始化数据提供者
-     */
-    private void initDataProVider()
-    {
+    private void initDataProVider() {
         mWeatherProvider = new WeatherProvider();
     }
 
 
-    /**
-     * 按下返回键
-     */
-    @Override
-    public void onBackPressed()
-    {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START))
-        {
-            drawer.closeDrawer(GravityCompat.START);
+    private void loadWeatherData() {
+
+        if (!checkIsNetAvailable()) {
+            return;
         }
-        else
-        {
+
+        showProgressDialog("更新天气信息...");
+        //当前天气
+        mWeatherProvider.loadWeatherNow("ip", new OnResponseListener() {
+            @Override
+            public void onBefore() {
+            }
+
+            @Override
+            public void onSuccess(Object response) {
+                fillWeatherNow((WeatherNow) response);
+            }
+
+            @Override
+            public void onFailure() {
+            }
+        });
+
+        //生活指数
+        mWeatherProvider.loadWeatherSuggestion("ip", new OnResponseListener() {
+            @Override
+            public void onBefore() {
+            }
+
+            @Override
+            public void onSuccess(Object response) {
+                fillWeatherSuggestion((WeatherSuggestion) response);
+                dismissProgressDialog();
+            }
+
+            @Override
+            public void onFailure() {
+                dismissProgressDialog();
+            }
+        });
+    }
+
+
+    private void fillWeatherNow(WeatherNow response) {
+        WeatherNow.ResultsBean result = response.getResults()
+                                                .get(0);
+        mTvCityName.setText(result.getLocation()
+                                  .getName());
+        mTvTemperature.setText(result.getNow()
+                                     .getTemperature());
+        mTvWeatherName.setText(result.getNow()
+                                     .getText());
+    }
+
+
+    private void fillWeatherSuggestion(WeatherSuggestion response) {
+        WeatherSuggestion.ResultsBean.SuggestionBean result = response.getResults()
+                                                                      .get(0)
+                                                                      .getSuggestion();
+        mTvSport.setText("运动指数: " + result.getSport()
+                                          .getBrief());
+        mTvFlu.setText("流感指数: " + result.getFlu()
+                                        .getBrief());
+        mTvCarWashing.setText("洗车指数: " + result.getCar_washing()
+                                               .getBrief());
+        mTvUv.setText("紫外线强度: " + result.getUv()
+                                        .getBrief());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
             super.onBackPressed();
         }
     }
@@ -126,16 +190,12 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
      */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item)
-    {
+    public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        if (id == R.id.nav_camera)
-        {
+        if (id == R.id.nav_camera) {
 
-        }
-        else if (id == R.id.nav_gallery)
-        {
+        } else if (id == R.id.nav_gallery) {
 
         }
 
@@ -143,17 +203,14 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         return true;
     }
 
-    public static void actionStart(Context context)
-    {
+    public static void actionStart(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
         context.startActivity(intent);
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
+    public void onClick(View v) {
+        switch (v.getId()) {
 
             case R.id.rl_tab_cook:
 
