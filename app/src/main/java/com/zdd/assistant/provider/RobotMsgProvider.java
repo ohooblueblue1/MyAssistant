ef090988898cce259ce34e6c03815b143154f977
ohooblueblue <helloworldzhuang@foxmail.com>
2017-02-19 21:59:31 +0800

    图灵机器人网络封装

--- app/src/main/java/com/zdd/assistant/provider/RobotMsgProvider.java
+++ app/src/main/java/com/zdd/assistant/provider/RobotMsgProvider.java
@@ -1,5 +1,16 @@
 package com.zdd.assistant.provider;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.zdd.assistant.app.MyApplication;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.bmob.v3.BmobUser;
+
 /**
  * Project Name: MyAssistant
  * File Name:    RobotMsgProvider.java
@@ -12,8 +23,42 @@ package com.zdd.assistant.provider;
 public class RobotMsgProvider {
 
     //图灵机器人API key
-    public static final String API_KEY = "04bdf8e0e7fb9ab48e1d6aa65bfec773";
+    public static final String API_KEY = "c9e91825064d4ccda8893b3733686592";
+
+    public static final String API_URL = "http://www.tuling123.com/openapi/api";
+
+    /**
+     * @param info     要发送的消息内容
+     * @param listener
+     */
+    public void sendMessage(String info, final OnResponseListener listener) {
+        listener.onBefore();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, API_URL, getParams(info), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                listener.onSuccess(jsonObject);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                listener.onFailure();
+            }
+        });
+        MyApplication.getRequestQueue().add(request);
+    }
 
 
+    private JSONObject getParams(String info) {
+        //根据输入的用户名密码包装参数
+        JSONObject msgObject = new JSONObject();
+        try {
+            msgObject.put("key",API_KEY);
+            msgObject.put("info",info);
+            msgObject.put("userid", BmobUser.getCurrentUser().getUsername());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return msgObject;
+    }
 
 }
