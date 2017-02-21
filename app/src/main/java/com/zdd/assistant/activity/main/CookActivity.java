263c9203d2683060a0b05571d2a7241afdd0b9f9
庄冬冬 <zhuangdongdong@4399inc.com>
2017-02-21 20:31:46 +0800

    菜谱初步实现

--- app/src/main/java/com/zdd/assistant/activity/main/CookActivity.java
+++ app/src/main/java/com/zdd/assistant/activity/main/CookActivity.java
@@ -3,20 +3,125 @@ package com.zdd.assistant.activity.main;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zdd.assistant.R;
 import com.zdd.assistant.base.BaseActivity;
+import com.zdd.assistant.entity.cook.CookList;
+import com.zdd.assistant.provider.CookProvider;
+import com.zdd.assistant.provider.OnResponseListener;
+import com.zdd.assistant.util.TextUtil;
+import com.zdd.assistant.util.ToastUtil;
 
-public class CookActivity extends BaseActivity {
+public class CookActivity extends BaseActivity implements View.OnClickListener
+{
+
+    private EditText mEdtSearch;
+    private TextView mBtnSearch;
+    private RecyclerView mRvCook;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook);
+
+        initView();
+
+        t1();
     }
 
-    public static void actionStart(Context context) {
+    private void t1()
+    {
+        new CookProvider().searchCook("鱼香肉丝", new OnResponseListener()
+        {
+            @Override
+            public void onBefore()
+            {
+
+            }
+
+            @Override
+            public void onSuccess(Object response)
+            {
+                CookList c = (CookList) response;
+
+                ToastUtil.showToast(CookActivity.this, c.getList()
+                                                        .get(0)
+                                                        .getInfo());
+            }
+
+            @Override
+            public void onFailure()
+            {
+
+            }
+        });
+    }
+
+    private void initView()
+    {
+        //初始化Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mEdtSearch = (EditText) findViewById(R.id.edt_search);
+        mBtnSearch = (TextView) findViewById(R.id.btn_search);
+        mBtnSearch.setOnClickListener(this);
+
+    }
+
+    //Toolbar菜单点击事件监听
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+            //按下返回导航按钮，结束当前Activity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static void actionStart(Context context)
+    {
         Intent intent = new Intent(context, CookActivity.class);
         context.startActivity(intent);
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn_search:
+                tryToSearchCook();
+                break;
+        }
+    }
+
+    private void tryToSearchCook()
+    {
+        if (TextUtil.isEmpty(mEdtSearch))
+        {
+            ToastUtil.showToast(this, "请先输入您要查询的内容哦~");
+            return;
+        }
+        if (!checkIsNetAvailable())
+        {
+            return;
+        }
+        // TODO: 发起请求
     }
 }
---
+++ app/src/main/java/com/zdd/assistant/provider/CookProvider.java
@@ -0,0 +1,70 @@
+package com.zdd.assistant.provider;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.zdd.assistant.app.MyApplication;
+import com.zdd.assistant.entity.cook.CookList;
+import com.zdd.assistant.util.ToastUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Project Name: MyAssistant
+ * File Name:    CookProvider.java
+ * ClassName:    CookProvider
+ *
+ * Description: 菜谱信息提供者
+ *
+ * @author ZDD
+ * @date 2017年02月21日 20:12
+ */
+public class CookProvider
+{
+    //图灵机器人API key
+    public static final String API_KEY = "c9e91825064d4ccda8893b3733686592";
+
+    public static final String API_URL = "http://www.tuling123.com/openapi/api";
+
+    public void  searchCook(String info, final OnResponseListener listener){
+        listener.onBefore();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, API_URL, getParams(info), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                if(jsonObject.optInt("code") != 308000){
+                    ToastUtil.showToast(MyApplication.getAppContext(),"对不起，未查找到您要的菜谱信息");
+                    return;
+                }
+                Gson gson = new Gson();
+                CookList result = gson.fromJson(jsonObject.toString(), CookList.class);
+                listener.onSuccess(result);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                listener.onFailure();
+            }
+        });
+        MyApplication.getRequestQueue().add(request);
+    }
+
+
+    private JSONObject getParams(String info) {
+        //根据输入的用户名密码包装参数
+        JSONObject msgObject = new JSONObject();
+        try {
+            msgObject.put("key",API_KEY);
+            msgObject.put("info",info);
+            msgObject.put("userid", BmobUser.getCurrentUser().getUsername());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return msgObject;
+    }
+
+}
